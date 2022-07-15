4e70f3852c08ab7824889ab695369a334d058753
Ethan Zonca <ez@ethanzonca.com>
2022-07-15 19:34:34 -0400

    Fix parsed argument usage. Reduce sleep time for performance. Add configuration of TTL at command line.

--- canblaster.py
+++ canblaster.py
@@ -8,28 +8,28 @@ import argparse
 
 class CANblaster(object):
 
-    def __init__(self, can_interface, bind_port, bind_ip):
+    def __init__(self, can_interface, bind_port, bind_ip, ttl):
 
         # Connect to CAN interface
         # Future: enable FD frames with CAN_RAW_FD_FRAMES
         self.can_socket = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
         self.can_socket.setblocking(1)
         self.can_socket.settimeout(0.1)
-        self.can_socket.bind((args.interface,))
+        self.can_socket.bind((can_interface,))
 
 
         # UDP socket for listening for heartbeats of clients
         self.udpserver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.udpserver.setblocking(0)
         self.udpserver.settimeout(0)
-        self.udpserver.bind((args.bind_ip, args.port))
+        self.udpserver.bind((bind_ip, bind_port))
 
 
         # Multicast socket, broadcast for discovery
         self.multicast_group = ('239.255.43.21', 20000)
         self.multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.multicast_socket.settimeout(0.2)
-        ttl = struct.pack('b', 1)
+        ttl = struct.pack('b', ttl)
         self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 
@@ -128,7 +128,7 @@ class CANblaster(object):
 
 
             # TODO: Remove once we are selecting on CAN RX
-            time.sleep(0.1)
+            time.sleep(0.001)
 
 
 
@@ -137,7 +137,7 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser(description='Serve local socketcan traffic over UDP')
-    parser.add_argument('interface', type=str,
+    parser.add_argument('can_interface', type=str,
                         help='local CAN port, such as can0')
     parser.add_argument('-p', '--port', type=int,
                         help='local UDP port to listen on',
@@ -145,8 +145,11 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--bind-ip', type=str,
                         help='IP address to listen on',
                         default='0.0.0.0')
+    parser.add_argument('-t', '--ttl', type=int,
+                        help='TTL for multicast discovery beacon',
+                        default=1)
 
     args = parser.parse_args()
 
-    b = CANblaster(args.interface, args.port, args.bind_ip)
+    b = CANblaster(args.can_interface, args.port, args.bind_ip, args.ttl)
     b.begin()
